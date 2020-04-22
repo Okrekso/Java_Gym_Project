aef6d137c59f2eee4c613c913f79fcf5e585dab6
okrekso <semikoseker@gmail.com>
2020-04-22 20:46:48 +0300

    • Most of GymDB functionality was moved into Database class

--- src/main/java/database/GymDB.java
+++ src/main/java/database/GymDB.java
@@ -17,22 +17,7 @@ public class GymDB extends Database {
     public GymDB() {
         super("jdbc:h2:mem:gymdb", "PUBLIC", "");
     }
-    @Override
-    public DBEntity addToTable(DBEntity entity) {
-        this.insertIntoTable(entity.getTableID(), entity.getColumns(false, false),
-                entity.getVariables(false));
-        try {
-            List<DBEntity> entities = getFromEntityTable(entity.getFactory());
-            return entities.get(entities.size()-1);
-        } catch (ParseException | SQLException e) {
-            log.error(e);
-            return null;
-        }
-    }
 
-    public String getTableCreationQuery(DBEntity entity) {
-        return "CREATE TABLE "+entity.getTableID()+"("+entity.getColumns(true, true)+")";
-    }
 
     @Override
     public boolean isDBcreated() {
@@ -53,39 +38,5 @@ public class GymDB extends Database {
                 new MemberFactory(),
                 new VisitFactory()
         );
-    }
-
-    public IDBEntityFactory getFactoryByTableName(String tableID) {
-        List<IDBEntityFactory> factories = getDBEntityFactories().stream()
-                .filter(factory -> factory.create().getTableID().toLowerCase().equals(tableID.toLowerCase()))
-                .collect(Collectors.toList());
-        if(factories.size()>0)
-            return factories.get(0);
-        else
-            return null;
-    }
-
-    public boolean build() {
-        List<String> queries = getDBEntities().stream()
-                .map(dbEntity -> getTableCreationQuery(dbEntity))
-                .collect(Collectors.toList());
-        for(String query : queries) {
-            executeUpdate(query);
-        }
-        return addToTable(new Info(0, new Date(), "DB creation")) != null;
-    }
-
-    public boolean dropCurrentDB() {
-        GymDB db = new GymDB();
-        List<String> tableIDs = getDBEntities().stream()
-                .sorted(
-                        (e1, e2) -> Boolean.compare(e2.hasForeignKeys(), e1.hasForeignKeys())
-                )
-                .map(dbEntity -> dbEntity.tableID).collect(Collectors.toList());
-        for(String tableID : tableIDs) {
-            if(!db.executeUpdate("DROP TABLE " + tableID))
-                return false;
-        }
-        return true;
     }
 }
